f224e92e0152a4142921bdd32507e3d703855f32
Helen Manders-Jones <realhelenmjones@gmail.com>
2022-07-01 09:49:51 +0100

    extract DownloadController.groovy - part II

--- grails-app/controllers/uk/org/nbn/downloads/DownloadController.groovy
+++ grails-app/controllers/uk/org/nbn/downloads/DownloadController.groovy
@@ -15,7 +15,56 @@ class DownloadController extends au.org.ala.downloads.DownloadController{
     }
 
     def options2(NbnDownloadParams downloadParams) {
-        request.setAttribute("mapLayoutParams",downloadParams.mapLayoutParams)
+
+
+        if (downloadParams.downloadType == DownloadType.MAP.type) {
+            downloadParams.email = authService?.getEmail() ?: downloadParams.email // if AUTH is not present then email should be populated via input on page
+
+            def queryContext = grailsApplication.config.biocache?.queryContext?:""
+            if (queryContext > '') {
+                downloadParams.searchParams = downloadParams.searchParams + '&fq=' + URLEncoder.encode(queryContext, "UTF-8")
+            }
+            def json = downloadService.triggerDownload(downloadParams)
+            log.info "map download json = ${json}"
+            chain (action:'confirm', model: [
+                    isQueuedDownload: true,
+                    downloadParams: downloadParams,
+                    json: json // Map
+            ], params:[searchParams: downloadParams.searchParams, targetUri: downloadParams.targetUri, downloadType: downloadParams.downloadType, mapLayoutParams: downloadParams.mapLayoutParams])
+
+        }
         super.options2(downloadParams)
+    }
+
+    def confirm (DownloadParams downloadParams) {
+
+        Map returnModel = super.confirm(downloadParams)
+        if (chainModel){
+            if (downloadParams.downloadType == DownloadType.CHECKLIST.type) {
+                chainModel.downloadUrl = replaceFacet(chainModel.downloadUrl)
+            } else if (downloadParams.downloadType == DownloadType.FIELDGUIDE.type) {
+                chainModel.downloadUrl = replaceFacet(chainModel.downloadUrl)
+                chainModel.json = replaceFacet(chainModel.json)
+            }
+        }
+
+        returnModel;
+
+    }
+
+    protected def replaceFacet(String url){
+        String facetToReplace = "species_guid"
+        if (grailsApplication.config.downloads?.checklistFacet) {
+            if (grailsApplication.config.downloads.checklistFacet != facetToReplace) {
+                String stringToReplace = "&facets=" + facetToReplace
+
+                def lastIndex = url.lastIndexOf(stringToReplace)
+                if (lastIndex > 0) {
+                    url = url.substring(0, lastIndex) + "&facets=" + grailsApplication.config.downloads.checklistFacet + url.substring(lastIndex + stringToReplace.length(), url.length())
+                }
+            }
+        }
+
+        return url
     }
 }
--- src/test/groovy/uk/org/nbn/downloads/DownloadControllerSpec.groovy
+++ src/test/groovy/uk/org/nbn/downloads/DownloadControllerSpec.groovy
@@ -1,22 +1,101 @@
 package uk.org.nbn.downloads
 
+import au.org.ala.downloads.DownloadParams
+import au.org.ala.downloads.DownloadType
+import au.org.ala.downloads.DownloadService
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
+
 @TestFor(DownloadController)
 class DownloadControllerSpec extends Specification {
 
+    private static String ALA_DOWNLOAD_URL = "https://records-ws.nbnatlas.org/occurrences/facets/download?file=records-2022-07-01&reasonTypeId=10&dwcHeaders=true&fileType=csv&qa=none&email=h.manders.jones%40nbn.org.uk&q=lsid%3ANBNSYS0000138878&fq=license%3A%22OGL%22&facets=species_guid&lookup=true&counts=true&lists=true"
+    private static String NBN_DOWNLOAD_URL = "https://records-ws.nbnatlas.org/occurrences/facets/download?file=records-2022-07-01&reasonTypeId=10&dwcHeaders=true&fileType=csv&qa=none&email=h.manders.jones%40nbn.org.uk&q=lsid%3ANBNSYS0000138878&fq=license%3A%22OGL%22&facets=taxon_concept_lsid&lookup=true&counts=true&lists=true"
+
+    private static String ALA_DOWNLOAD_URL2 = "https://records-ws.nbnatlas.org/occurrences/facets/download?file=records-2022-07-01&reasonTypeId=10&dwcHeaders=true&fileType=csv&qa=none&email=h.manders.jones%40nbn.org.uk&q=lsid%3ANBNSYS0000138878&fq=license%3A%22OGL%22&facets=species_guid"
+    private static String NBN_DOWNLOAD_URL2 = "https://records-ws.nbnatlas.org/occurrences/facets/download?file=records-2022-07-01&reasonTypeId=10&dwcHeaders=true&fileType=csv&qa=none&email=h.manders.jones%40nbn.org.uk&q=lsid%3ANBNSYS0000138878&fq=license%3A%22OGL%22&facets=taxon_concept_lsid"
+
+    private static String ALA_BASE_DOWNLOAD_URL = "https://records-ws.nbnatlas.org/occurrences/facets/download?file=records-2022-07-01&reasonTypeId=10&dwcHeaders=true&fileType=csv&qa=none&email=h.manders.jones%40nbn.org.uk&q=lsid%3ANBNSYS0000138878&fq=license%3A%22OGL%22"
+
     def setup() {
+//        Holders.grailsApplication.config.downloads.checklistFacet = "taxon_concept_lsid"
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "test replace facet=species_guid when config.downloads.checklistFacet defined"() {
+        given:
+            grailsApplication.config.downloads.checklistFacet = 'taxon_concept_lsid'
+
+        expect:
+            controller.replaceFacet(ALA_DOWNLOAD_URL) == NBN_DOWNLOAD_URL
+    }
+
+    void "test replace facet=species_guid when config.downloads.checklistFacet is not defined"() {
+        given:
+        grailsApplication.config.downloads.checklistFacet = null
+
+        expect:
+        controller.replaceFacet(ALA_DOWNLOAD_URL) == ALA_DOWNLOAD_URL
+    }
+
+    void "test replace facet=species_guid when config.downloads.checklistFacet is species_uid"() {
+        given:
+        grailsApplication.config.downloads.checklistFacet = "species_guid"
+
+        expect:
+        controller.replaceFacet(ALA_DOWNLOAD_URL) == ALA_DOWNLOAD_URL
+    }
+
+    void "test options2 for CHECKLIST type"(){
+        setup:
+        grailsApplication.config.downloads.checklistDownloadUrl = ALA_BASE_DOWNLOAD_URL
+        grailsApplication.config.downloads.checklistFacet = 'taxon_concept_lsid'
+
+        def downloadParams = Mock(DownloadParams)
+        downloadParams.downloadType >> DownloadType.CHECKLIST.type
+        downloadParams.reasonTypeId >> "wotev"
+        downloadParams.biocacheDownloadParamString() >> ""
+        downloadParams.email >> "a@a.com"
+
+
+        when:
+        controller.options2(downloadParams)
+        controller.confirm(downloadParams)
+
+        then:
+        controller.chainModel.downloadUrl == NBN_DOWNLOAD_URL
+
+    }
+
+    void "test options2 for FIELDGUIDE type"(){
+        setup:
+        grailsApplication.config.downloads.fieldguideDownloadUrl = ALA_BASE_DOWNLOAD_URL
+        grailsApplication.config.downloads.checklistFacet = 'taxon_concept_lsid'
+
+        controller.downloadService = Mock(DownloadService)
+        controller.downloadService.triggerFieldGuideDownload(_) >> ALA_DOWNLOAD_URL2
+
+        def downloadParams = Mock(DownloadParams)
+        downloadParams.downloadType >> DownloadType.FIELDGUIDE.type
+        downloadParams.reasonTypeId >> "wotev"
+        downloadParams.biocacheDownloadParamString() >> ""
+        downloadParams.email >> "a@a.com"
+
+
+        when:
+        controller.options2(downloadParams)
+        controller.confirm(downloadParams)
+
+        then:
+        controller.chainModel.downloadUrl == NBN_DOWNLOAD_URL2
+        controller.chainModel.json == NBN_DOWNLOAD_URL2
+
     }
 }
